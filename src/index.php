95371d0a1e09318a7ecd6c8183a2054bb19c4d2c
Robson Melo <melodisouza@gmail.com>
2021-08-05 20:25:47 -0300

    feature subtracao dos numeros

--- src/index.php
+++ src/index.php
@@ -11,13 +11,14 @@ $climate = new CLImate();
 $padding = $climate->padding(20);
 
 $padding->label('Somar Valores')->result('[1]');
+$padding->label('Subtrair Valores')->result('[2]');
 $padding->label('Sair')->result('[q]');
 
 $climate->br();
 
 $input = $climate->input('Selecione uma opção do MENU');
 
-$input->accept([1, 'q']);
+$input->accept([1, 2, 'q']);
 $input->strict();
 
 $response = $input->prompt();
@@ -32,8 +33,21 @@ switch($response) {
 
         $soma = $valorPrimeiroNumero + $valorSegundoNumero;
 
-        $climate->output(sprintf('Soma %s com %s é igual: %s .', $valorPrimeiroNumero, $valorSegundoNumero, $soma));
+        $climate->output(sprintf('Soma %s com %s é igual: %s ', $valorPrimeiroNumero, $valorSegundoNumero, $soma));
         break;
+
+    case '2':
+        $primeiroNumero = $climate->input('Primeiro valor:');
+        $valorPrimeiroNumero =$primeiroNumero->prompt();
+
+        $segundoNumero = $climate->input('Segundo valor:');
+        $valorSegundoNumero = $segundoNumero->prompt();
+
+        $subtrair = $valorPrimeiroNumero - $valorSegundoNumero;
+
+        $climate->output(sprintf('Subtração de %s com %s é igual: %s ', $valorPrimeiroNumero, $valorSegundoNumero, $subtrair));
+        break;
+
         default:
         $climate->out('Sistema encerrado.');
 }
